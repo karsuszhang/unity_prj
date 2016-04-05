75dce52cb4fef4b77b3d75b806e6494f3ac2dd4d
karsuszhang <yifanzhang@10.0.0.12>
2016-04-05 22:51:43 +0800

    create

---
+++ TestUnityPrj/Assets/Scripts/Main.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Test4Json
+{
+	public List<int> list_test = new List<int>();
+	public Dictionary<int, string> dic_test = new Dictionary<int, string>();
+	public string string_test = "this is test";
+}
+
+public class Main : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Test4Json t = new Test4Json ();
+		t.list_test.Add (4);
+		t.dic_test [1] = "test1";
+
+		string ret = JsonUtility.ToJson (t);
+		Debug.Log (ret);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
