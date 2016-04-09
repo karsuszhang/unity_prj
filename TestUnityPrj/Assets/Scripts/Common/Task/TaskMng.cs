4231fd77a0729210a32fb4135bc11c5e2570244c
karsuszhang <yifanzhang@10.0.0.12>
2016-04-09 12:00:31 +0800

    task framework

---
+++ TestUnityPrj/Assets/Scripts/Common/Task/TaskMng.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace CommonUtil
+{
+	public delegate bool TaskFunc();
+	public delegate IEnumerable CoroutineTask();
+	
+	public class TaskMng : MonoBehaviour
+	{
+		#region static
+		static TaskMng _instance;
+		public static TaskMng Instance
+		{
+			get {
+				if (_instance == null) {
+					GameObject go = new GameObject ("CoroutineObject");
+					GameObject.DontDestroyOnLoad (go);
+					_instance = go.AddComponent<TaskMng> ();
+				}
+				return _instance;
+			}
+		}
+		#endregion
+
+		private TaskMng()
+		{
+			
+		}
+
+		void Update()
+		{
+		}
+	}
+}
---
+++ TestUnityPrj/Assets/Scripts/Common/Task/TaskObj.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Threading;
+
+namespace CommonUtil
+{
+	public class TaskObj
+	{
+		Thread m_Thread = null;
+
+		public void Start (TaskFunc fun)
+		{
+			if (m_Thread == null)
+				m_Thread = new Thread (TaskUpdate);
+
+			//m_Thread
+		}
+
+		void TaskUpdate()
+		{
+		}
+	}
+}
